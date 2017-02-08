8da65e6ee556f94cae8c2522b60d08d88cf1f256
jmlopezt <juanto1514@gmail.com>
2017-02-08 18:10:05 +0100

    License Header
    fixed

--- ADSB_Server/src/Server/WorkerRunnable.java
+++ ADSB_Server/src/Server/WorkerRunnable.java
@@ -1,20 +1,20 @@
-/*
- *  ADSB_Server is a simple multithreaded server which accepts a client
- *  connection and then is proccesed by a worker thread.
- *
- *  2017  Juan Manuel López Torralba & Manuel Moya Ferrer
- *
- *  This work is licensed under the Creative Commons
- *  Attribution-NonCommercial-ShareAlike 
- *  CC BY-NC-SA
- *  License 
- *  
- * This license lets others remix, tweak, and build upon your work
- * non-commercially, as long as they credit you and license their new 
- * creations under the identical terms.
- *
- * To view a copy of the license, visit https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
- */
+///////////////////////////////////////////////////////////////////
+///////////  ADS-B PlaneTracker Raspberry-Android /////////////////
+///////////////////////////////////////////////////////////////////
+///////////               Authors:                      ///////////  
+//       Manuel Moya Ferrer - Juan Manuel López Torralba         //
+//                                                               //
+//  OpenSource with Creative Commons Attribution-NonCommercial-  //
+//  ShareAlike license ubicated in GitHub throughout:            //
+// https://github.com/mmoyaferrer/PlaneTracker-Android-Raspberry //
+//                                                               //
+//  This work is licensed under the Creative Commons             //
+//  Attribution-NonCommercial-ShareAlike CC BY-NC-SA License.    //
+//  To view a copy of the license, visit                         //
+//  https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode  //
+///////////                                             ///////////
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
 
 
 
